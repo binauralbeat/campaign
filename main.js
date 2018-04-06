06e9a008a45f5110e8318524ba6d368e922fbe96
binauralbeat <binauralbeat001@gmail.com>
2018-04-06 12:35:40 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,191 @@
+const candidate = Object.create({}, {
+    dateCreated: {
+        enumerable: true,
+        writable: false,
+        value: "04/06/2018"
+    },
+    candidateName: {
+        enumerable: true,
+        writable: false,
+        value: "Elizabeth Sanger"
+    },
+    partyAff: {
+        enumerable: true,
+        writable: false,
+        value: "Democrat"
+    },
+    age: {
+        enumerable: true,
+        writable: false,
+        value: "54"
+    },
+    congressDist: {
+        enumerable: true,
+        writable: false,
+        value: "5th"
+    },
+   
+})
+console.log(candidate)
+
+const platformStatements = Object.create({}, {
+    dateCreated: {
+        enumerable: true,
+        writable: false,
+        value: "04/06/2018"
+    },
+    taxPos: {
+        enumerable: true,
+        writable: false,
+        value: "no taxes"
+    },
+    jobs: {
+        enumerable: true,
+        writable: false,
+        value: "universal income/jobs are obsolete now that robots & A.I. rule the workforce"
+    },
+    infrastructure: {
+        enumerable: true,
+        writable: false,
+        value: "infrastructure needs little to no maintinence as robots are constantly repairing and improving it"
+    },
+    healthcare: {
+        enumerable: true,
+        writable: false,
+        value: "strong advocate for universal healthcare as no one can get insurance through workplace since no one has a job anymore"
+    },
+    crimeEnforcement: {
+        enumerable: true,
+        writable: false,
+        value: "robots do that"
+    },
+   
+})
+
+console.log(platformStatements)
+
+const donate = Object.create({}, {
+
+donationUrl: {
+    enumerable: true,
+    writable: false,
+    value: "https://donotdonate.com/robotsrunthisshit"
+},
+
+})
+
+console.log(donate)
+
+const calendar = Object.create({}, {
+
+    roadTripCali: {
+        enumerable: true,
+        writable: false,
+        value: "5-5-2018"
+    },
+
+    pizzaPartay: {
+        enumerable: true,
+        writable: false,
+        value: "7-3-2018"
+
+    },
+
+})
+
+console.log(calendar)
+
+const jeffCrosby = Object.create({},{
+    
+    fears: {
+        enumerable: true,
+        writable: false,
+        value: "YMCA locker room"
+    },
+    favouriteFood: {
+        enumerable: true,
+        writeable: false,
+        value: "Kimchi"
+    },
+    skills: {
+        enumerable: true,
+        writable: false,
+        value: "excels at tapdance and paper mache"
+    },
+    name: {
+        enumerable: true,
+        writable: true,
+        value: "Jdawg"
+    },
+    number: {
+        enumerable:true,
+        writable: false,
+        value: "1"
+    },
+
+})
+
+console.log(jeffCrosby)
+
+const bio = Object.create({},{
+
+    birth: {
+        enumerable: true,
+        writable: false,
+        value: "immaculate conception"
+    },
+
+    birthDate: {
+        enumerable: true,
+        writable: false,
+        value: "when time stood still"
+
+    },
+})
+
+console.log(bio)
+
+const imageGal = Object.create({},{
+
+    headShot: {
+        enumerable: true,
+        writable: false,
+        value: "pic of head"
+    },
+    famPic: {
+        enumerable: true,
+        writable: false,
+        value: "pic of fam"
+    },
+    constPic: {
+        enumerable: true,
+        writable: false,
+        value: "pic of constituents"
+    },
+})
+
+console.log(imageGal)
+
+const missionStatement = Object.create({},{
+
+    stateIt: {
+        enumerable: true,
+        writable: false,
+        value: "go for it!"
+    },
+
+
+})
+
+console.log(missionStatement)
+
+const vote = Object.create({}, {
+
+    voterUrl: {
+        enumerable: true,
+        writable: false,
+        value: "htts://bff.com"
+    },
+})
+
+console.log(vote)
